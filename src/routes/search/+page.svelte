8622b9e9be7581bde0b9cdb89ad9dbf936abd49f
Almuhanad-1 <almuhanad.alnihmy@gmail.com>
2023-12-14 21:20:20 +0300

    Add search functionality to API and search page

---
+++ src/routes/search/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import ListingCard from '$lib/components/ListingCard.svelte';
+	import ListingCardContainer from '$lib/components/ListingCardContainer.svelte';
+	import type { PageData } from './$types';
+	export let data: PageData;
+
+	$: ({ listings } = data);
+	console.log(listings);
+</script>
+
+<button on:click={() => goto('/')}>Go Back</button>
+
+<section>
+	<ListingCardContainer>
+		{#each listings as listing}
+			<ListingCard {listing} />
+		{/each}
+	</ListingCardContainer>
+</section>
