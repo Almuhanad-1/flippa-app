44f2471e7cc9984c17aea645f10dc50c224726b8
Almuhanad-1 <almuhanad.alnihmy@gmail.com>
2023-12-14 21:20:26 +0300

    Refactor page component to use ListingCard and ListingCardContainer components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,66 +1,35 @@
 <script lang="ts">
+	import ListingCard from '$lib/components/ListingCard.svelte';
+	import ListingCardContainer from '$lib/components/ListingCardContainer.svelte';
+	import SearchInput from '$lib/components/SearchInput.svelte';
 	import type { PageData } from './$types';
 	export let data: PageData;
-	$: ({ results, page } = data);
+	$: ({ listings, page } = data);
 
 	async function showMoreHandler() {
 		console.log(page, 'page');
-		const newResults = await fetch(`/api/getResults?page=${page + 1}`).then((res) => {
+		const newListings = await fetch(`/api/getResults?page=${page + 1}`).then((res) => {
 			page = page + 1;
 			return res.json();
 		});
-		results = [...results, ...newResults];
+		listings = [...listings, ...newListings];
 	}
 </script>
 
+<SearchInput />
+
 <section>
-	<ul>
-		{#each results as result}
-			<li>
-				<a href={`/${result.id}`}>
-					<h3>{result.basic_info.name}</h3>
-					<p>{result.title}</p>
-					<p>{result.price.toLocaleString()}$</p>
-				</a>
-			</li>
+	<ListingCardContainer>
+		{#each listings as listing}
+			<ListingCard {listing} />
 		{/each}
-	</ul>
+	</ListingCardContainer>
 	<div class="showmore-container">
 		<button on:click={showMoreHandler}>Show more</button>
 	</div>
 </section>
 
 <style>
-	ul {
-		list-style: none;
-		padding: 0;
-	}
-
-	li {
-		margin-bottom: 1rem;
-		background-color: #555;
-		border-radius: 0.5rem;
-		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-		padding: 1rem;
-		color: #eee;
-		display: flex;
-		flex-direction: column;
-	}
-
-	a {
-		text-decoration: none;
-		font-size: 1.2rem;
-	}
-
-	li:hover {
-		transform: scale(1.01);
-	}
-
-	h3 {
-		margin: 0 0 0.5rem;
-		font-weight: 500;
-	}
-
 	button {
 		background-color: #eee;
 		border: none;
